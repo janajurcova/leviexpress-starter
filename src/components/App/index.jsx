b03b2b96f1513a7d614a7750b06cf15cad0cd3a8
Jana Jurčová <jj.jurcova@gmail.com>
2022-05-21 11:09:47 +0200

    add reservation Route

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservation" element={<Reservation />} />
+    </Routes>
     <Footer />
-  </>
+  </BrowserRouter>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Reservation = () => {
+    return (
+        <main>
+            <h2>Detail jízdenky</h2>
+        </main>
+    );
+};
